2de01ad3a6be151dafc1b3ce172dd9072986b397
LaukkuPaukku <81646967+LaukkuPaukku@users.noreply.github.com>
2023-10-08 23:22:33 +0300

    new versions of files with basic SQL functionality
    After much trouble (I bravely but foolishly had chosen to use Cygwin as my terminal app, the course has no tutorial for it and I had to Google a LOT of arcane errors) I got SQL working. Here is the example app in the course material, adjusted to work on my computer.

--- app.py
+++ app.py
@@ -1,32 +1,26 @@
 from flask import Flask
-from flask import render_template
-import glob
+from flask import redirect, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import text
 
 app = Flask(__name__)
-
-#path = r'templates/*.html'
-files = glob.glob("./templates/*.html")
-filenames=[]
-for filestr in files:
-    filenames.append("http://127.0.0.1:5000/"+filestr[filestr.replace("\\","/").rfind("/")+1:])
-    #    filenames.append("<a href=\"http://127.0.0.1:5000/"+filestr[filestr.replace("\\","/").rfind("/")+1:]+"\">link</a>"
-#print(filenames)
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///postgres"
+db = SQLAlchemy(app)
 
 @app.route("/")
 def index():
-    return render_template("index.html", items=filenames)
+    result = db.session.execute(text("SELECT content FROM messages"))
+    messages = result.fetchall()
+    return render_template("index.html", count=len(messages), messages=messages) 
 
-@app.route("/<string:id>")
-def page(id):
-    if id=="index.html":
-        return index()
-    else:
-        return render_template(id)
+@app.route("/new")
+def new():
+    return render_template("new.html")
 
-#@app.route("/post/<int:id>")
-#def page(id):
-#    return render_template("post"+ str(id)+".html")
-#
-#@app.route("/post1")
-#def post1():
-#    return render_template("post1.html")
+@app.route("/send", methods=["POST"])
+def send():
+    content = request.form["content"]
+    sql = "INSERT INTO messages (content) VALUES (:content)"
+    db.session.execute(text(sql), {"content":content})
+    db.session.commit()
+    return redirect("/")
